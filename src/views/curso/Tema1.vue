5d021d7228c4fef5a49a4a1fe45c295dcfc08848
Francisco Lizcano <flizcanor@icloud.com>
2025-07-10 23:41:37 -0500

    tema 1

--- src/views/curso/Tema1.vue
+++ src/views/curso/Tema1.vue
@@ -3,8 +3,215 @@
   BannerInterno(:subTitulo="'1. Título tema'")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
 
+    .row.mb-5
+      .col-lg-8
+        .bg-color-1.p-4.mb-4(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/1.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Jean Piaget (1896 – 1980), fue un biólogo pionero de la psicología evolutiva y es considerado el padre de la epistemología genética. Sus estudios estuvieron enmarcados en el período de vida de la infancia, a través de la argumentación de la teoría cognoscitiva y constructivista, sobre el desarrollo de la inteligencia en los niños. Su teoría enfatiza sobre la forma como se construyen o desarrollan las formas de entender el mundo desde los niños, aplicando sus capacidades de pensar, razonar e interactuar con su entorno. 
+    
+        p(data-aos="fade-left") Por lo anterior, a Jean Piaget se le atribuyan las #[b etapas del desarrollo cognitivo], que son momentos de la vida del niño y la niña, que van de acuerdo con su edad: 
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/2.png", data-aos="zoom-in")
+
+
+    .row.align-items-start.mb-5
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/3.png", alt="").mb-4.mb-lg-0
+      .col-lg-8
+    
+        AcordionA(tipo="b")
+          .div(titulo="Fase sensoriomotora (0-2 años)")
+            p Los bebés aprenden a través de sus sentidos y movimientos. Desarrollan la comprensión de la permanencia del objeto, es decir, que los objetos existen incluso cuando no están visibles.
+    
+          .div(titulo="Etapa preoperacional (2-7 años)")
+            p Los niños comienzan a usar el lenguaje y el pensamiento simbólico. Su razonamiento es egocéntrico y tienen dificultades para entender conceptos abstractos.
+    
+          .div(titulo="Etapa de operaciones concretas (7-11 años)")
+            p Los niños empiezan a pensar lógicamente sobre eventos concretos. Pueden clasificar y ordenar objetos, su pensamiento aún está limitado a situaciones tangibles.
+    
+          .div(titulo="Etapa de operaciones formales (12 años en adelante)")
+            p Se desarrolla el pensamiento abstracto, permitiendo a los adolescentes razonar sobre situaciones hipotéticas y resolver problemas complejos.
+
+
+    .bg-full-width.bg-color-4.mb-5
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-auto
+            img(src="@/assets/curso/tema1/4.svg", style="max-width: 90px").mx-auto
+          .col-lg
+            p.mb-0 Piaget usó en su teoría la noción de #[b transformaciones], que hacen alusión a los cambios de las personas, en todo momento de la vida. Por su parte, las etapas explican las condiciones, características o cualidades que van presentando las personas, niños en el proceso de transformación. Por lo anterior, la teoría del desarrollo cognitivo explica la forma como los procesos mentales se van complejizando y el sujeto hace procesos de reorganización progresiva, según la etapa.
+
+
     #t_1_1.titulo-segundo(data-aos="flip-up")
-      h2 #[span 1.1] Subtítulo tema
+      h2 #[span 1.1] Inteligencia operativa e inteligencia figurativa
+
+    p.mb-4 Dentro de las explicaciones claves de Piaget en su teoría, están:
+
+
+    .bg-full-width.bg-color-7.mb-3
+      .px-4.px-md-5.py-4
+
+        .row.justify-content-center.align-items-stretch
+          .col-lg-6.mb-4(data-aos="zoom-in-up")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/5.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center La inteligencia operativa
+                p.mb-0 Se trata de las acciones que le permiten al individuo anticiparse, recuperar o identificar objetos o personas, es decir “encajar” sobre modelos que ya están en su mente.
+          .col-lg-6.mb-4(data-aos="zoom-in-down")
+            .custom-image-card-2.h-100
+              img.custom-image-card__image(src="@/assets/curso/tema1/6.png" alt="")
+              .custom-image-card__text.p-4
+                h5.mb-2.text-center La inteligencia figurativa
+                p.mb-0 Es la capacidad que tiene el individuo de representar los aspectos “estáticos” de la realidad o los estadíos, e incluye la percepción, imitación, imágenes mentales y lenguajes. Esta inteligencia está determinada por los aspectos que derivan de la inteligencia operativa.
+
+
+    .bg-full-width.bg-color-3.mb-5
+      .px-4.px-md-5.py-4.py-lg-5
+        p.mb-0 De acuerdo con lo anterior, se puede deducir que la inteligencia operativa va en aumento conforme crecen el niño y la niña y en el estadío de las operaciones formales; ellos son capaces de utilizar la lógica para construir conclusiones, resolver problemas y construir hipótesis.
+
+
+    #t_1_2.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.2] Asimilación y acomodación
+
+    .row.mb-5
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/7.png", data-aos="zoom-in")
+      .col-lg-8
+        p(data-aos="fade-left").mb-4 Para Piaget (1964), en la #[b asimilación], el individuo incorpora funcionalmente aspectos del medio y actúa con o sobre él, para transformarlo. Un ejemplo asociado es cuando un niño conoce un perro, quizá la raza del perro sea pequeña, de pelo crespo y de color blanco. En cuanto el niño ve otro animal como un caballo o un gato, que también está parado en sus cuatro patas, un ejercicio de asimilación sería que, también, lo reconozca como perro. 
+    
+        .bg-color-1.p-4(data-aos="fade-left")
+          p.mb-0 En el caso de la #[b acomodación], el niño modifica sus estructuras cognitivas para poder incorporar esa estructura cognoscitiva (gato o caballo), con sus propias cualidades y particularidades y lo deja de asumir como perro, como en el caso de la asimilación.
+
+
+    #t_1_3.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.3] Características de la teoría de desarrollo cognitivo
+
+    .row.mb-5
+      .col-lg-8
+        .bg-color-2.p-4(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/8.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Piaget resaltó la importancia de la niñez (primera infancia), en los procesos de aprendizaje inicial y para toda la vida; también explicó que el desarrollo cognitivo es un proceso asociado a las etapas del desarrollo y, por ende, está asociada a la edad e implica una reorganización progresiva de procesos y habilidades mentales.
+        .bg-color-2.p-4.mb-4(data-aos="fade-left")
+          .row.align-items-start
+            .col-lg-auto
+              img(src="@/assets/curso/tema1/9.svg", style="max-width: 90px").mx-auto
+            .col-lg
+              p.mb-0 Por lo tanto, se puede concluir que los niños pueden ser caracterizados en esas etapas o estadíos y a medida que se da el crecimiento, pueden ir construyendo su comprensión del mundo, con mayores niveles de complejidad.
+    
+        p(data-aos="fade-left") Lo anterior implica que el niño y la niña deben interactuar con objetos, acercarse a su mundo y reconocer fenómenos del entorno. Así las cosas, el desarrollo cognitivo es el resultado de la maduración biológica y la experiencia del niño y la niña con su entorno y condiciones.
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/10.png", data-aos="zoom-in")
+
+
+    .titulo-figura.mb-2
+      h5 Figura 1. 
+      span Interacción de la maduración biológica y las experiencias del entorno inciden en el desarrollo cognitivo
+    
+    figure.bg-color-info.p-4.p-md-5
+      img(src="@/assets/curso/tema1/f1.svg", data-aos="zoom-in", style="max-width: 495px").mx-auto
+    .bg-color-4.p-4.mb-5
+      .row.align-items-center
+        .col-lg-auto
+          img(src="@/assets/curso/tema1/11.svg", style="max-width: 90px").mx-auto
+        .col-lg
+          p.mb-0 Por otro lado, la teoría también detalla que los niños siguen la acción de imitación, es decir, que replican acciones que ven en adultos y las asimilan, de tal manera que, cuando se construye el concepto de dicho objeto, pueden usarlo para interpretar de nuevo otro escenario; de allí la adaptación a través de la asimilación y acomodación.
+
+    #t_1_4.titulo-segundo(data-aos="flip-up")
+      h2 #[span 1.4] ¿Qué aplicaciones se pueden hacer de la teoría de Piaget, a los contextos actuales de la primera infancia?
+
+    .row.mb-5
+      .col-lg-8
+        .bg-color-1.px-4.py-5(data-aos="fade-left").mb-4
+          p.mb-0 La teoría de Piaget ha inspirado a muchos maestros y padres de familia, que reconocen cómo el medio rico en aprendizajes y elementos de interacción, incide en los procesos de maduración cognitiva.
+        
+        p(data-aos="fade-left") Definir y caracterizar las etapas de desarrollo, ha sido un aporte significativo para orientar a cientos de profesores y cuidadores para establecer parámetros de acción pedagógica, construir materiales pedagógicos y didácticos pertinentes, e identificar situaciones en las que el aprendizaje está en riesgo por situaciones biológicas, cognitivas o sociales, del contexto particular. 
+
+        p(data-aos="fade-left") Al respecto, algunas recomendaciones que emergen de la teoría y que pueden ser aplicadas en contextos escolares y familiares:
+
+    
+      .col-lg-4
+        figure
+          img(src="@/assets/curso/tema1/12.png", data-aos="zoom-in")
+
+
+    .bg-full-width.bg-fondo-slider
+      .p-4.p-md-5
+        SlyderA(tipo="b").bg-white
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/tema1/slide-1.png")
+            .col-lg-7
+              p Diseñar y motivar experiencias de aprendizaje por descubrimiento en los niños y niñas, en entornos escolares y familiares.
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/tema1/slide-2.png")
+            .col-lg-7
+              p Implementar procesos de diagnóstico que faciliten la caracterización del estado del niño según su edad y madurez biológica. También que permita tomar decisiones sobre la ruta didáctica a utilizar.
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/tema1/slide-3.png")
+            .col-lg-7
+              p Propiciar la interacción con pares (niños y niñas de la misma edad) y nutrir su relacionamiento con actividades colaborativas, que promuevan la empatía, la comunicación y el respeto.
+          .row.align-items-center.p-4.p-md-5
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/tema1/slide-4.png")
+            .col-lg-7
+              p Orientar a los niños y niñas para que puedan construir soluciones a situaciones cotidianas. Esto implica permitirles analizar situaciones reales, identificando causa y efecto o consecuencias.
+          .row.align-items-center.p-4.p-md-5.mb-0
+            .col-lg-5
+              figure
+                img(src="@/assets/curso/tema1/slide-5.png")
+            .col-lg-7
+              p Diseñar materiales que les permita a los niños, hacer acciones de pensamiento según su edad, con estrategias didácticas que permitan el desarrollo de habilidades variadas.
+        
+    
+    
+
+    
+    
+    
+    
+    
+
+    
+
+        
+
+    
+
+    
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
 
     .bg-full-width.border-top.color-primario
       .p-4.p-md-5
@@ -30,18 +237,3 @@
               img(src='@/assets/componentes/material-complementario.svg', alt='')
 
 </template>
-
-<script>
-export default {
-  name: 'Tema1',
-  data: () => ({}),
-
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-}
-</script>
-
-<style lang="sass"></style>
