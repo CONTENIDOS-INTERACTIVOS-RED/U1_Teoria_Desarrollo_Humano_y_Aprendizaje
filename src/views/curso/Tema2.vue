96ade14c25eb5cfb46eedb59c5251ddcae0fa348
Francisco Lizcano <flizcanor@icloud.com>
2025-07-11 00:02:37 -0500

    Tema 2

--- src/views/curso/Tema2.vue
+++ src/views/curso/Tema2.vue
@@ -1,45 +1,195 @@
 <template lang="pug">
   .curso-main-container.pb-3
-    BannerInterno(:subTitulo="'2. Título tema'")
+    BannerInterno(:subTitulo="'2. Perspectiva socio cultural Vygostski'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5.overflow-hidden
   
+      .row.mb-5
+        .col-lg-8
+          p(data-aos="fade-left").mb-4 Lev Semyonovich Vygotski, fue un sociólogo que generó aportes a la psicología del desarrollo y la neuropsicología; es considerado el fundador de la psicología histórico-cultural. Dentro de sus conclusiones, afirmó que la comunidad y el lenguaje juegan un papel central en el aprendizaje. 
+      
+          .bg-color-1.p-4(data-aos="fade-left").mb-4
+            .row.align-items-start
+              .col-lg-auto
+                img(src="@/assets/curso/tema2/1.svg", style="max-width: 90px").mx-auto
+              .col-lg
+                p.mb-0 Una de las diferencias con la teoría de Piaget, es que Vygostski describe que el desarrollo de los niños es independiente de etapas específicas y, en su lugar, da un papel preponderante a las interacciones sociales como acciones que fomentan el pensamiento y la cognición. Adicionalmente, Vygotski hizo aportes en el campo de la psicología evolutiva sobre la aparición del lenguaje y la comunicación dentro del desarrollo social y cognitivo de la primera infancia. 
+      
+      
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/2.png", data-aos="zoom-in")
+      
       #t_2_1.titulo-segundo(data-aos="flip-up")
-        h2 #[span 2.1] Subtítulo tema
+        h2 #[span 2.1] Pensamiento y lenguaje
+      
+      .row.mb-5
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/3.png", data-aos="zoom-in")
+        .col-lg-8
+          .bg-color-2.p-4(data-aos="fade-left").mb-4
+            p.mb-0 Según Carrera y Mazzarella (2001), uno de los aportes más significativos de Vygotsky es la relación que establece entre el pensamiento y el lenguaje. Aunque desde la genética, la expresión de estas dos cualidades son distintas y provienen de un origen genómico distinto, en la infancia hay un momento ambas cualidades convergen y entonces el pensamiento se torna verbal y el lenguaje racional.
+      
+          p(data-aos="fade-left").mb-4 Desde el ámbito de la interacción social, el niño y la niña deben o desean expresar su experiencia, por tanto, se les hace necesario y útil construir un sistema como si se tratara de un “prototipo” del lenguaje. De allí que la experiencia de la primera infancia tiene elementos simbólicos que utilizan los infantes para entender y comunicar los procesos sociales. Esa creación de signos, tiene un método y requiere el procesamiento de información (creación y uso).
 
-      .bg-full-width.border-top.color-primario
-        .p-4.p-md-5
-          h2(data-aos="fade-left") MATERIAL COMPLEMENTARIO
-          .row.material-complementario
-            .col-12.col-md-6.col-lg-7
-              p Los invitamos a explorar el material complementario de este curso, en esta sección encontrará recursos que le permitirán profundizar  y enriquecer su aprendizaje en los temas tratados en esta unidad.
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/componentes/link.svg' :style="{'max-width':'16px'}")
-                a(href="https://elibro.net/es/lc/tecnologicadeloriente/titulos/274611" target="_blank" rel="noopener noreferrer") De Castro Emilio Cerdá, F. J. A. L. M. (s/f). Las energías renovables en el ámbito internacional. Revistasice.com. 
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/template/book.svg' :style="{'max-width':'16px'}")
-                span De Janeiro, R. (1992). La declaración del Río sobre el Medio Ambiente y el Desarrollo. Río de Janeiro República Federativa del Brasil., aprobada en Estocolmo el, 16.
-  
-              p.d-flex.my-4
-                img.me-3(src='@/assets/template/icono-yt.svg' :style="{'max-width':'16px'}")
-                a(href="https://youtu.be/QKLjwCMyv3M?si=uAWhO9tnyghljaJg" target="_blank" rel="noopener noreferrer") Innovación y sostenibilidad.
-  
-            .col-12.col-md-6.col-lg-3.offset-lg-1
+
+      #t_2_2.titulo-segundo(data-aos="flip-up")
+        h2 #[span 2.2] Zona de desarrollo próximo
+
+
+      .bg-full-width.bg-color-5
+        .px-4.px-md-5.py-4
+
+          .row.mb-4
+            .col-lg-8
+              .bg-color-1.px-4.py-5(data-aos="fade-left").mb-4
+                p.mb-0 Dentro de las cualidades de esa interacción, se encuentra la relevancia del mentor, que usualmente es un adulto o cuidador, que guía al niño o la niña, en su proceso de acercamiento con el mundo o zona de desarrollo próximo, que describe la diferencia entre lo que un infante puede hacer sin ayuda y lo que no puede hacer. La mediación para Vygostski es una forma de cooperación para el aprendizaje que conduce a la construcción de conocimientos.
+              
+              p(data-aos="fade-left") De acuerdo con la teoría, los humanos nacen con cuatro funciones elementales: 
+
+            .col-lg-4.d-none.d-sm-block
               figure
-                img(src='@/assets/componentes/material-complementario.svg', alt='')
-  
+                img(src="@/assets/curso/tema2/4.png", data-aos="zoom-in").mb-4.mb-lg-0
+
+
+          .row.justify-content-center.align-items-stretch.mb-4
+            .col-lg-3
+              .bg-color-6.p-4(data-aos="fade-left").h-100
+                .row.align-items-center
+                  .col-lg-auto
+                    img(src="@/assets/curso/tema2/5.svg", style="max-width: 90px").mx-auto
+                  .col-lg
+                    h5.mb-2 Atención
+
+          
+            .col-lg-3
+              .bg-color-6.p-4(data-aos="fade-left").h-100
+                .row.align-items-center
+                  .col-lg-auto
+                    img(src="@/assets/curso/tema2/6.svg", style="max-width: 90px").mx-auto
+                  .col-lg
+                    h5.mb-2 Sensación
+          
+            .col-lg-3
+              .bg-color-6.p-4(data-aos="fade-left").h-100
+                .row.align-items-center
+                  .col-lg-auto
+                    img(src="@/assets/curso/tema2/7.svg", style="max-width: 90px").mx-auto
+                  .col-lg
+                    h5.mb-2 Percepción
+          
+            .col-lg-3
+              .bg-color-6.p-4(data-aos="fade-left").h-100
+                .row.align-items-center
+                  .col-lg-auto
+                    img(src="@/assets/curso/tema2/8.svg", style="max-width: 90px").mx-auto
+                  .col-lg
+                    h5.mb-2 Memoria
+      
+      
+      p.mb-4 Estas funciones elementales se van desarrollando y complejizando, logrando funciones superiores o su desarrollo óptimo; pero ¿Cómo se logra tal desarrollo?, la teoría afirma que es a través de la interacción social, en la que la interlocución con el otro, con la mediación a través del lenguaje y el uso de símbolos, permite que se produzcan tales procesos superiores. 
+
+      .titulo-figura.mb-2
+        h5 Figura 1. 
+        span Funciones mentales elementales con las que nace el humano, según Vygostski
+
+      figure.bg-color-info.p-4.p-md-5.mb-5
+        img(src="@/assets/curso/tema2/f2.svg", data-aos="zoom-in", style="max-width: 365px").mx-auto
+      
+      #t_2_3.titulo-segundo(data-aos="flip-up")
+        h2 #[span 2.3] La interacción social 
+
+
+      .row.mb-5
+        .col-lg-4
+          figure
+            img(src="@/assets/curso/tema2/9.png", data-aos="zoom-in")
+        .col-lg-8
+          p(data-aos="fade-left").mb-4 La interacción social es un proceso fundamental desde la niñez y para toda la vida; es un proceso mediante el cual las personas se relacionan entre sí, intercambian información, emociones y comportamientos, y se influyen mutuamente. 
+      
+          .bg-color-1.p-4(data-aos="fade-left")
+            p.mb-0 Es un fenómeno crucial para el desarrollo humano, la formación de relaciones y la construcción de la sociedad. La interacción social implica el intercambio de información y emociones entre individuos, y se puede realizar a través de la comunicación verbal y no verbal.
+
+
+      .bg-full-width.bg-color-9
+        .px-4.px-md-5.py-4
+          .row.align-items-center
+            .col-lg
+              p.mb-4 Dentro de los tipos de interacciones humanas, se encuentran:
+
+              ul.lista-ul--color.color-dark.mb-4
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Interacción persona – persona.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Interacción persona – grupo.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Interacción grupo – grupo.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Cooperación.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Conflicto social.
+                li.mb-0
+                  i.fas.fa-arrow-circle-right
+                  | Intercambio.
+
+              
+
+      
+            .col-lg-auto
+              img(src="@/assets/curso/tema2/10.svg", style="max-width: 651px").mx-auto
+
+      .bg-full-width.bg-color-2.mb-5
+        .px-4.px-md-5.py-4
+          .row.align-items-center
+            .col-lg-auto
+              img(src="@/assets/curso/tema2/11.svg", style="max-width: 651px").mx-auto
+            .col-lg
+              p.mb-0 La interacción social es vital para el desarrollo emocional, social y cognitivo de la persona y los grupos; además fomenta los vínculos personales y profesionales, la cohesión social, el bienestar psicológico y la empatía. 
+
+
+      #t_2_4.titulo-segundo(data-aos="flip-up")
+        h2 #[span 2.4] ¿Qué aplicaciones se pueden hacer de la teoría de Vygotsky a los contextos actuales de la primera infancia?
+
+      p.mb-4 Entendiendo la postura de que el aprendizaje tiene un componente clave de interacción en un entorno social y promueve un aprendizaje colaborativo y mediado por el lenguaje, a continuación, se detallan algunas estrategias que pueden ser aplicadas en los contextos educativos, escolares o familiares:
+
+      .bg-full-width.bg-fondo-slider.mb-5
+        .p-4.p-md-5
+          SlyderA(tipo="b").bg-white
+            .row.align-items-center.p-4.p-md-5
+              .col-lg-5
+                figure
+                  img(src="@/assets/curso/tema2/slide-1.png")
+              .col-lg-7
+                p Los niños y niñas deben ser y sentirse protagonistas de su propio proceso de aprendizaje. Aunque la interacción suele hacerse con adultos cuidadores (padres, familiares, maestros), es clave que se brinde el espacio de interacción con niños de su misma edad.
+            .row.align-items-center.p-4.p-md-5
+              .col-lg-5
+                figure
+                  img(src="@/assets/curso/tema2/slide-2.png")
+              .col-lg-7
+                p Promover la estimulación de la zona de desarrollo próximo, a partir de juegos y actividades de interacción.
+            .row.align-items-center.p-4.p-md-5
+              .col-lg-5
+                figure
+                  img(src="@/assets/curso/tema2/slide-3.png")
+              .col-lg-7
+                p Diseñar e implementar actividades que vayan teniendo niveles de complejidad creciente.
+            .row.align-items-center.p-4.p-md-5
+              .col-lg-5
+                figure
+                  img(src="@/assets/curso/tema2/slide-4.png")
+              .col-lg-7
+                p Disponer algunas tareas de trabajo colaborativo, con problemas por resolver y la promoción de respuestas basadas en la interacción.
+      
+
+
+      
+      
+
+
+
   </template>
-
-<script>
-export default {
-  name: 'Tema2',
-  mounted() {
-    this.$nextTick(() => {
-      this.$aosRefresh()
-    })
-  },
-}
-</script>
-
-<style lang="sass"></style>
